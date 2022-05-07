783cb764d35fd2bb94950cac51894aeac4c397ad
benjaminbascary <benjaminbascary@gmail.com>
2022-05-06 22:39:33 -0300

    Setted up the currencies component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,13 +3,14 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import CartModal from '../CartModal/CartModal';
 import "./Dashboard.css";
+import Currencies from '../Currencies/Currencies';
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props)
     this.state = {
       products : {...props},
-      currency : {},
+      currencies : this.props.currencies,
       cart : [],
       showShop: true,
       showPreCart: false,
@@ -20,38 +21,40 @@ export default class Dashboard extends Component {
 
   addProductToCart = (id) => {
     this.state.cart.push(id)
-  }
+  };
 
   toggleShow = () => {
     this.setState(({showShop}) => ({showShop: !showShop}))
-  }
+  };
 
   setActive = () => {
     this.setState(({active}) => ({active: !active}))
-  }
+  };
 
   updateCounter = () => {
     this.setState(prev => ({ counter: prev.counter + 1 }))
-  }
+  };
 
   render() {
     return (
       <div>
-        {console.log(this.props.currencies)}
         <div className='header-container'>
           <div className='title-container'>
             <h1 className='header-title'>Scandiweb Store</h1>
           </div>
-          <div>
+          <div className='button-container' onClick={this.toggleShow}>
+            <p className='button-container-text'>{this.state.showShop ? "CHECK OUT" : "CONTINUE SHOPPING"}</p>
+          </div>
+          <div className='button-currencies-container'>
+            <Currencies currencies={this.props.currencies}/>
+            {this.state.active? <CartModal active={this.state.active} toggle={this.setActive} children={<div>hola</div>}/> : ""}
+            <div>
             <div className='cart-product-counter'>
               {this.state.counter}
             </div>
             <img className="header-cart-button" onClick={this.setActive} src="./resources/cart.png" alt="pre-cart"></img>
           </div>
-          {this.state.active? <CartModal active={this.state.active} toggle={this.setActive} children={<div>hola</div>}/> : ""}
-          <div className='button-container' onClick={this.toggleShow}>
-            <p className='button-container-text'>{this.state.showShop ? "CHECK OUT" : "CONTINUE SHOPPING"}</p>
-          </div>
+        </div>
         </div>
         {
         this.state.showShop?
@@ -63,6 +66,7 @@ export default class Dashboard extends Component {
                           props={eachProduct}
                           updateCounter={this.updateCounter}
                           add={this.addProductToCart}
+                          symbol={this.state.currencies[0].symbol} //como prueba le estoy pasando el simbolo numero [0] del array de simbolos
                         />
               })}
             </div>
